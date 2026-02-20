a8dfe608f3eccb3e29424928134e4f9738b438d8
Oscar Neveux <oscar.neveux@gmail.com>
2026-02-20 14:17:00 -0500

    feat: Add Order, ShippingInformation, CreditCard and Transaction models

--- models.py
+++ models.py
@@ -21,9 +21,52 @@ class Product(BaseModel):
     class Meta:
         table_name = "products"
 
+class Order(BaseModel):
+    email = CharField(null=True)
+    product = ForeignKeyField(Product, backref="orders")
+    quantity = IntegerField()
+    paid = BooleanField(default=False)
+
+    class Meta:
+        table_name = "orders"
+
+class ShippingInformation(BaseModel):
+    order = ForeignKeyField(Order, backref="shipping_information", unique=True)
+    country = CharField()
+    address = CharField()
+    postal_code = CharField()
+    city = CharField()
+    province = CharField()
+
+    class Meta:
+        table_name = "shipping_information"
+
+
+class CreditCard(BaseModel):
+    order = ForeignKeyField(Order, backref="credit_card", unique=True)
+    name = CharField()
+    first_digits = CharField()
+    last_digits = CharField()
+    expiration_year = IntegerField()
+    expiration_month = IntegerField()
+
+    class Meta:
+        table_name = "credit_cards"
+
+
+class Transaction(BaseModel):
+    order = ForeignKeyField(Order, backref="transaction", unique=True)
+    transaction_id = CharField()
+    success = BooleanField()
+    amount_charged = IntegerField()
+
+    class Meta:
+        table_name = "transactions"
+
+
 def initialize_db():
     db.connect(reuse_if_open=True)
     db.create_tables(
-        [Product],
+        [Product, Order, ShippingInformation, CreditCard, Transaction],
         safe=True,
     )
